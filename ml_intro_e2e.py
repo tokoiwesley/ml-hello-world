78f17cff015b5491b65f72a7bdb7695cd43f59e0
Wesley Tokoi <tokoiwesley@gmail.com>
2021-08-05 22:40:07 +0300

    Load dataset

--- ml_intro_e2e.py
+++ ml_intro_e2e.py
@@ -14,3 +14,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+
+# Load dataset
+# url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+url = "./iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
